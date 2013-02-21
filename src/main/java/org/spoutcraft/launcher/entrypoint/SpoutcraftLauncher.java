decc429955ab5beb89949df45be17aac085108d2
dwi <omg@upal.se>
2013-02-21 05:38:02 +0100

    No longer requires faces to load before launching. Caches faces as well.
    Signed-off-by: Dalton Pelc <daltonjpelc@gmail.com>

--- src/main/java/org/spoutcraft/launcher/entrypoint/SpoutcraftLauncher.java
+++ src/main/java/org/spoutcraft/launcher/entrypoint/SpoutcraftLauncher.java
@@ -156,6 +156,8 @@ public class SpoutcraftLauncher {
 
 		PackManager.addRestPacks(selector);
 		PackManager.addCustomPacks(selector);
+		
+		frame.updateFaces();
 
 		if (params.hasAccount()) {
 			frame.disableForm();
