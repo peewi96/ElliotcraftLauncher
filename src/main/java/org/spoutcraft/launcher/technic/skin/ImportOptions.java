6a5b6c19999fda8c64aba8ddc4244c7969150160
dwi <omg@upal.se>
2013-02-26 19:43:02 +0100

    Cleaned up escape key commit
    Signed-off-by: Dalton Pelc <daltonjpelc@gmail.com>

--- src/main/java/org/spoutcraft/launcher/technic/skin/ImportOptions.java
+++ src/main/java/org/spoutcraft/launcher/technic/skin/ImportOptions.java
@@ -63,9 +63,10 @@ public class ImportOptions extends JDialog implements ActionListener, MouseListe
 	private static final String IMPORT_ACTION = "import";
 	private static final String CHANGE_FOLDER = "folder";
 	private static final String PASTE_URL = "paste";
+	private static final String ESCAPE_ACTION = "escape";
 	private static final int FRAME_WIDTH = 520;
 	private static final int FRAME_HEIGHT = 222;
-	private static final String CLOSEDIALOG_KEY = "ESCAPE";
+
 	private JLabel msgLabel;
 	private JLabel background;
 	private LiteButton save;
@@ -93,16 +94,18 @@ public class ImportOptions extends JDialog implements ActionListener, MouseListe
 		Font fontregular = MetroLoginFrame.getClassicFont(13);
 		Font fontbold = MetroLoginFrame.getClassicBoldFont(13);
 		
-		//  Handle escape key to close the dialog
 		KeyStroke escape = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0, false);
-		Action escapeAction = new AbstractAction()
-		{ public void actionPerformed(ActionEvent e)
-			{
+		Action escapeAction = new AbstractAction() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
 				dispose();
 			}
 		};
-		getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(escape, CLOSEDIALOG_KEY);
-		getRootPane().getActionMap().put(CLOSEDIALOG_KEY, escapeAction);
+
+		getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(escape, ESCAPE_ACTION);
+		getRootPane().getActionMap().put(ESCAPE_ACTION, escapeAction);
 
 		background = new JLabel();
 		background.setBounds(0,0, FRAME_WIDTH, FRAME_HEIGHT);
