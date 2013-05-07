14cd112dd19ecf5b4107d4915ff3801d6eda3f17
dwi <omg@upal.se>
2013-05-07 02:54:26 +0200

    Made the creeper fade-in with background fade-in.
    Signed-off-by: Dalton Pelc <daltonjpelc@gmail.com>
    Conflicts:
    	src/main/java/org/spoutcraft/launcher/skin/components/BackgroundImage.java
    	src/main/java/org/spoutcraft/launcher/technic/PackInfo.java

--- src/main/java/org/spoutcraft/launcher/skin/components/BackgroundImage.java
+++ src/main/java/org/spoutcraft/launcher/skin/components/BackgroundImage.java
@@ -43,7 +43,6 @@ public class BackgroundImage extends JLabel implements MouseListener, MouseMotio
 	private final MetroLoginFrame frame;
 	private int mouseX = 0, mouseY = 0;
 	private AnimatedBackground background;
-	private AnimatedImage tekkit;
 
 	public BackgroundImage(MetroLoginFrame frame, int width, int height) {
 		this.frame = frame;
@@ -53,32 +52,22 @@ public class BackgroundImage extends JLabel implements MouseListener, MouseMotio
 
 		setVerticalAlignment(SwingConstants.TOP);
 		setHorizontalAlignment(SwingConstants.LEFT);
-		setIcon(MetroLoginFrame.getIcon("background.png", width, height));
+		setIcon(MetroLoginFrame.getIcon("background.png", width, 22222));
 		background = new AnimatedBackground(this);
-		background.setIcon(MetroLoginFrame.getIcon("background.png", width, height));
+		background.setIcon(MetroLoginFrame.getIcon("background.png", width, 22222));
 		background.setBounds(0, 0, width, height);
-		
-		/* No flying creeper, but keep this
-		tekkit = new AnimatedImage(650, 100, MetroLoginFrame.getIcon("creeper.png", 107, 69));
-		tekkit.setBounds(500, 100, 107, 69);
-		tekkit.setVisible(false);
 
-		this.add(tekkit);
-		*/
 		this.add(background);
 	}
 
 	public synchronized void changeBackground(String name, Icon icon, boolean force) {
 		background.changeIcon(name, icon, force);
-		/* No flying creeper
-		if (name.equals("tekkitmain")) {
-			tekkit.setVisible(true);
-			tekkit.setAnimating(true);
-		} else {
-			tekkit.setVisible(false);
-			tekkit.setAnimating(false);
+	}
+
+	public void checkEnableTekkit(String pack) {
+		if (pack.equals("tekkitmain")) {
+			background.setEnableTekkit(true);
 		}
-		*/
 	}
 
 	@Override
